4336e14407800fb53016f70366b6b6ed9bc2dcdc
the-code-machine <sarthak25ic049@satiengg.in>
2024-07-23 23:58:21 +0530

    update

--- src/components/utils/HeroSection.tsx
+++ src/components/utils/HeroSection.tsx
@@ -32,7 +32,7 @@ export const AuroraHero = () => {
             }}
             className=" w-full  flex justify-center items-center  xl:px-64 lg:px-20 md:px-8  bg-gray-950 px-4  xl:pt-36 lg:pt-24 md:pt-24 pt-28 xl:pb-12 lg:pb-12 md:pb-12 pb-12  xl:space-x-15 text-gray-200">
             <div className=' w-full flex flex-col justify-start '>
-                <Reveal width='100%'>  <h1 style={{ lineHeight: 1 }} className='xl:text-[4.5rem] lg:text-[3.5rem] md:text-[2.5rem] text-[2.5rem] font-bold  text-white'>MARKETING & PROMOTION</h1></Reveal>
+                <Reveal width='100%'>  <h1 style={{ lineHeight: 1 }} className='xl:text-[4.5rem] lg:text-[3.5rem] md:text-[2.5rem] text-[2.5rem] font-bold  text-white'>DIGITAL MARKETING</h1></Reveal>
                 <Reveal width='100%'> <div className=' px-3 py-2 border border-tertiary-200  text-white xl:w-[97%] lg:w-[87%] md:w-[80%] w-[100%]  font-light xl:text-[20px]  text-sm rounded-md my-5'>
                     Cutting-Edge Strategies, One Click Away.                </div></Reveal>
                 <Reveal width='100%'> <p className=' text-paragraph-200 font-light xl:text-lg  text-sm'>
